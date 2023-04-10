6244b80df36f12be620d9819e2f3c5ae895c7e6f
959 <53804059@qq.com>
2023-04-10 15:29:27 +0800

    修改

--- src/Facades/IM.php
+++ src/Facades/IM.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the hedeqiang/ten-im.
- *
- * (c) hedeqiang<laravel_code@163.com>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
-
 namespace TencentIM\Facades;
 
 use Illuminate\Support\Facades\Facade;
